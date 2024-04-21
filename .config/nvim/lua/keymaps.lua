7be64e714ea0c83f950b93c8236cc0a8f38aa106
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-04-21 12:15:40 +0300

    add keymap for colorcolumn

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,6 +2,15 @@ local map = vim.keymap.set
 
 map('n', '<leader>tn', '<cmd>set relativenumber!<CR>', { desc = '[T]oggle relative [N]umber lines' })
 
+map('n', '<leader>tc', function()
+  if vim.o.colorcolumn == '' then
+    vim.o.colorcolumn = '80,100'
+    vim.api.nvim_set_hl(0, 'ColorColumn', { bg = 'gray' })
+  else
+    vim.o.colorcolumn = ''
+  end
+end, { desc = '[T]oggle color [C]olumn' })
+
 map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
