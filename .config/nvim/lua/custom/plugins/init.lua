83df20ccedfe2a183d89676282afd3d384ba0976
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-06-03 10:16:30 +0300

    add telescope filebrowser

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -360,4 +360,11 @@ return {
       preview_window = true,
     },
   },
+  {
+    'nvim-telescope/telescope-file-browser.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+    keys = {
+      { '<leader>fb', '<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>', desc = '[F]ile [B]rowser' },
+    },
+  },
 }
