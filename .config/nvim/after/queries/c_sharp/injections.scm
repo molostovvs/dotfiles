8cff80dfd9a3a011e978800eb72946958f9153b5
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-09-29 11:45:57 +0300

    add treesitter injections for sql

---
+++ .config/nvim/after/queries/c_sharp/injections.scm
@@ -0,0 +1,10 @@
+;extends
+
+(
+  [
+    (string_literal_content)
+    (raw_string_content)
+  ] @injection.content
+  (#match? @injection.content "(SELECT|select|INESERT|insert|UPDATE|update|DELETE|delete|UPSERT|upsert|DECLARE|declare).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by)?")
+  (#set! injection.language "sql")
+)
--- .config/nvim/after/queries/cs/injections.scm
+++
@@ -1,4 +0,0 @@
-((
-  (raw_string_literal) @constant
-  (#match? @constant "(SELECT|select|insert|INSERT).*")
-)@injection.content (#set! injection.language "sql"))
