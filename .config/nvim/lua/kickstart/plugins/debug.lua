f9f7fc953b9343d623f26c62c2471db83dac5964
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-10-06 21:12:57 +0300

    add netcoredbg to debugger config

--- .config/nvim/lua/kickstart/plugins/debug.lua
+++ .config/nvim/lua/kickstart/plugins/debug.lua
@@ -22,7 +22,7 @@ return {
       automatic_setup = true,
       handlers = {},
       ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
+        'netcoredbg',
       },
     }
 
