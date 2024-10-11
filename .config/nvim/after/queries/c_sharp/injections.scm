d52d86642569d22117730089bf29e3fe53d105ba
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-10-11 18:05:17 +0300

    nvim: update tree-sitter queries

--- .config/nvim/after/queries/c_sharp/injections.scm
+++ .config/nvim/after/queries/c_sharp/injections.scm
@@ -1,10 +1,25 @@
-;extends
+; extends
 
+; var x = "str";
+(
+(comment) @comment
+.
+(_
+  (_
+    (_
+      (_
+        (string_literal_content) @injection.content))))
+
+(#eq? @comment "//language=sql")
+(#set! injection.language "sql")
+)
+
+; fallback
 (
   [
     (string_literal_content)
     (raw_string_content)
   ] @injection.content
-  (#match? @injection.content "(SELECT|select|INESERT|insert|UPDATE|update|DELETE|delete|UPSERT|upsert|DECLARE|declare).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by)?")
+  (#match? @injection.content "(SELECT|select|INSERT|insert|UPDATE|update|DELETE|delete|UPSERT|upsert|DECLARE|declare).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by)?")
   (#set! injection.language "sql")
 )
