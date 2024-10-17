efd19b117d672f0f2b4f9623381c1d329a557aac
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-10-17 20:08:21 +0300

    nvim: change default colorscheme

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -188,7 +188,12 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'github_dark_default'
+      require('github-theme').setup {
+        options = {
+          -- transparent = true,
+        },
+      }
+      vim.cmd.colorscheme 'github_dark_high_contrast'
     end,
   },
   {
