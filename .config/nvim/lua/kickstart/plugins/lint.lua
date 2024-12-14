3015d8ddac7b38da53c8f46715a9f81ce10e6eba
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-12-14 21:34:14 +0300

    nvim: refactor lint

--- .config/nvim/lua/kickstart/plugins/lint.lua
+++ .config/nvim/lua/kickstart/plugins/lint.lua
@@ -4,15 +4,22 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
-      lint.linters_by_ft = lint.linters_by_ft or {}
-      lint.linters_by_ft['markdown'] = { 'markdownlint' }
-      lint.linters_by_ft['json'] = { 'jsonlint' }
-      lint.linters_by_ft['sql'] = { 'sqlfluff' }
-      lint.linters_by_ft['lua'] = { 'luacheck' }
-      lint.linters_by_ft['terraform'] = { 'tflint' }
-      lint.linters_by_ft['dockerfile'] = { 'hadolint' }
-      lint.linters_by_ft['typescript'] = { 'eslint_d' }
-      lint.linters_by_ft['typescriptreact'] = { 'eslint_d' }
+      lint.linters_by_ft = {
+        markdown = { 'markdownlint' },
+        json = { 'jsonlint' },
+        sql = { 'sqlfluff' },
+        lua = { 'luacheck' },
+        terraform = { 'tflint' },
+        dockerfile = { 'hadolint' },
+        typescript = { 'eslint_d' },
+        typescriptreact = { 'eslint_d' },
+        ghaction = { 'actionlint' },
+      }
+
+      lint.linters.luacheck.args = {
+        '--globals',
+        'vim',
+      }
 
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
       vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
