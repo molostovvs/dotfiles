a511d52d6c062235cc32a31dff7ee3da9f2b3987
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-12-22 22:08:20 +0300

    i3: add nvidia overclock, add telegram workspace rule

---
+++ .config/i3/overclock.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# TODO: overclock only high performance levels
+nvidia-settings -a GPUGraphicsClockOffsetAllPerformanceLevels=155
+nvidia-settings -a GPUMemoryTransferRateOffsetAllPerformanceLevels=2000
