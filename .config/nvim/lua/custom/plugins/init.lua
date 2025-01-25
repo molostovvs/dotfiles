e9fb1decc96506a58408836e6adc9268b2d6d73b
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2025-01-26 02:00:34 +0300

    nvim: add ai autocomplete plugin again

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -757,4 +757,21 @@ return {
       }
     end,
   },
+  {
+    'monkoose/neocodeium',
+    event = 'VeryLazy',
+    config = function()
+      local neocodeium = require 'neocodeium'
+      neocodeium.setup()
+      vim.keymap.set('i', '<A-l>', function()
+        neocodeium.accept()
+      end)
+      vim.keymap.set('i', '<A-]>', function()
+        neocodeium.cycle_or_complete(1)
+      end)
+      vim.keymap.set('i', '<A-[>', function()
+        neocodeium.cycle(-1)
+      end)
+    end,
+  },
 }
