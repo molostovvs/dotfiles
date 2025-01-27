bd50befd0e338a4b3ece905f274e3972c19a83e6
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2025-01-27 21:53:20 +0300

    nvim: make neocodeium manual

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -761,7 +761,9 @@ return {
     event = 'VeryLazy',
     config = function()
       local neocodeium = require 'neocodeium'
-      neocodeium.setup()
+      neocodeium.setup {
+        manual = true,
+      }
       vim.keymap.set('i', '<A-l>', function()
         neocodeium.accept()
       end)
