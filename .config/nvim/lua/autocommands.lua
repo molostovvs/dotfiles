bed308c4fcc3d5c6cc0bc79e82b688d2c7034ad6
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2025-02-23 22:40:54 +0300

    nvim(refactor): refactor mason and lspconfig

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -11,3 +11,31 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+  callback = function(event)
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if client and client.server_capabilities.documentHighlightProvider then
+      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        buffer = event.buf,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+        buffer = event.buf,
+        callback = vim.lsp.buf.clear_references,
+      })
+    end
+
+    if client and client.server_capabilities.codeLensProvider then
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'InsertLeave' }, {
+        buffer = 0,
+        group = vim.api.nvim_create_augroup('lsp-autocommands', { clear = true }),
+        callback = function()
+          vim.lsp.codelens.refresh { bufnr = 0 }
+        end,
+      })
+    end
+  end,
+})
--- .config/nvim/lua/custom/plugins/lsp/mason.lua
+++ .config/nvim/lua/custom/plugins/lsp/mason.lua
@@ -1,3 +1,46 @@
+local servers = {
+  docker_compose_language_service = {},
+  roslyn = {},
+  fsautocomplete = {},
+  terraformls = {},
+  bashls = {},
+  jsonls = {},
+  lua_ls = {
+    settings = {
+      Lua = {
+        completion = {
+          callSnippet = 'Both',
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = { 'vim' },
+        },
+        hint = {
+          enable = true,
+        },
+      },
+    },
+  },
+}
+
+local tools = {
+  'stylua',
+  'jq',
+  'prettier',
+  'eslint_d',
+  'sqlfmt',
+  'jsonlint',
+  'markdownlint',
+  'netcoredbg',
+  'hadolint',
+  'luacheck',
+  'selene',
+  'tflint',
+  'actionlint',
+  'shfmt',
+  'fantomas',
+}
+
 return {
   {
     'williamboman/mason.nvim',
@@ -9,6 +52,28 @@ return {
       },
     },
   },
-  'williamboman/mason-lspconfig.nvim',
-  'WhoIsSethDaniel/mason-tool-installer.nvim',
+  {
+    'williamboman/mason-lspconfig',
+    dependencies = { 'saghen/blink.cmp', 'neovim/nvim-lspconfig', 'j-hui/fidget.nvim' },
+    opts = {
+      handlers = {
+        function(server_name)
+          local capabilities = require('blink.cmp').get_lsp_capabilities(nil, true)
+          capabilities.textDocument.foldingRange = {
+            dynamicRegistration = false,
+            lineFoldingOnly = true,
+          }
+          local server = servers[server_name] or {}
+          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+          require('lspconfig')[server_name].setup(server)
+        end,
+      },
+    },
+  },
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    opts = {
+      ensure_installed = vim.list_extend(vim.tbl_keys(servers), tools),
+    },
+  },
 }
