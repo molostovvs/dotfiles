f6ac5bd1219fe8dda64af1961e14bc1a4f4529a3
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2025-02-23 22:45:10 +0300

    fish: add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+alias dotui="gitui -d \$HOME/.cfg/ -w \$HOME"
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
