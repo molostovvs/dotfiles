22d54773422f31c2c9348e2625ff38d7d7fac8e4
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2025-03-16 15:16:29 +0300

    nvim(mnemonic): upd config

--- .config/nvim/lua/custom/plugins/tools/mnemonic.lua
+++ .config/nvim/lua/custom/plugins/tools/mnemonic.lua
@@ -1,6 +1,32 @@
 return {
   {
+    lazy = false,
     dir = '~/source/playground/mnemonic.nvim/',
+    ---@module 'mnemonic.init'
+    ---@type MnemonicConfig
     opts = {},
+    keys = {
+      {
+        'mt',
+        function()
+          require('mnemonic.ui').add_bookmark()
+        end,
+        desc = '[B]ookmark [C]reate',
+      },
+      {
+        'md',
+        function()
+          require('mnemonic.bookmarks').remove_bookmark()
+        end,
+        desc = '[B]ookmark [D]elete',
+      },
+      {
+        'mp',
+        function()
+          require('mnemonic.bookmarks').show_bookmarks()
+        end,
+        desc = '[B]ookmarks [P]rint',
+      },
+    },
   },
 }
