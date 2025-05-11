3b5b7636a42e2b2c80569a7e70729e5bca548706
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2025-05-11 10:35:22 +0300

    nvim(ui): move to kanagawa colorscheme

--- .config/nvim/lua/custom/plugins/ui/theme.lua
+++ .config/nvim/lua/custom/plugins/ui/theme.lua
@@ -17,6 +17,7 @@ return {
   },
   {
     'projekt0n/github-nvim-theme',
+    enabled = false,
     name = 'github-theme',
     lazy = false,
     priority = 1000,
@@ -30,6 +31,34 @@ return {
         },
       }
       vim.cmd.colorscheme 'github_dark_high_contrast'
+    end,
+  },
+  {
+    'rebelot/kanagawa.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('kanagawa').setup {
+        overrides = function()
+          return {
+            NormalFloat = { bg = 'none' },
+            FloatBorder = { bg = 'none' },
+            FloatTitle = { bg = 'none' },
+          }
+        end,
+        theme = 'wave',
+        transparent = true,
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = 'none',
+              },
+            },
+          },
+        },
+      }
+      vim.cmd 'colorscheme kanagawa-wave'
     end,
   },
 }
