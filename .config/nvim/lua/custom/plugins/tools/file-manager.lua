e21089359bfa54d28fbee6006b2103207f8fd6e8
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2025-05-30 23:57:05 +0300

    nvim(yazi): add yazi plugin

--- .config/nvim/lua/custom/plugins/tools/file-manager.lua
+++ .config/nvim/lua/custom/plugins/tools/file-manager.lua
@@ -2,12 +2,48 @@ return {
   {
     'stevearc/oil.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    enabled = true,
+    enabled = false,
     ---@module 'oil'
     ---@type oil.SetupOpts
     opts = {},
     keys = {
       { '<leader>fb', '<cmd>Oil<cr>', desc = '[F]ile [B]rowser' },
+    },
+  },
+  {
+    'mikavilpas/yazi.nvim',
+    event = 'VeryLazy',
+    ---@type YaziConfig | {}
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+      keymaps = {
+        show_help = '<f1>',
+      },
+    },
+    -- 👇 if you use `open_for_directories=true`, this is recommended
+    init = function()
+      -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+      -- vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+    end,
+    keys = {
+      {
+        '<leader>-',
+        mode = { 'n', 'v' },
+        '<cmd>Yazi<cr>',
+        desc = 'Open yazi at the current file',
+      },
+      {
+        '<leader>cw',
+        '<cmd>Yazi cwd<cr>',
+        desc = "Open the file manager in nvim's working directory",
+      },
+      {
+        '<c-up>',
+        '<cmd>Yazi toggle<cr>',
+        desc = 'Resume the last yazi session',
+      },
     },
   },
 }
