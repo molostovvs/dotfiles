59975bf600da3d5f823b5ccd52710ceaa45385eb
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2025-05-31 10:41:52 +0300

    nvim(yazi): change keybinds

--- .config/nvim/lua/custom/plugins/tools/file-manager.lua
+++ .config/nvim/lua/custom/plugins/tools/file-manager.lua
@@ -16,31 +16,31 @@ return {
     ---@type YaziConfig | {}
     opts = {
       -- if you want to open yazi instead of netrw, see below for more info
-      open_for_directories = false,
+      open_for_directories = true,
       keymaps = {
-        show_help = '<f1>',
+        show_help = '?',
       },
     },
     -- 👇 if you use `open_for_directories=true`, this is recommended
     init = function()
       -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-      -- vim.g.loaded_netrw = 1
+      vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
     end,
     keys = {
       {
-        '<leader>-',
+        '<leader>fb',
         mode = { 'n', 'v' },
         '<cmd>Yazi<cr>',
         desc = 'Open yazi at the current file',
       },
       {
-        '<leader>cw',
+        '<leader>fc',
         '<cmd>Yazi cwd<cr>',
         desc = "Open the file manager in nvim's working directory",
       },
       {
-        '<c-up>',
+        '<leader>fr',
         '<cmd>Yazi toggle<cr>',
         desc = 'Resume the last yazi session',
       },
