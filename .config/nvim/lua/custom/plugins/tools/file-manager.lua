3f68686c413d61497893bbfd79b1341445d3b560
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2025-07-03 17:26:43 +0300

    nvim: cleanup old plugins

--- .config/nvim/lua/custom/plugins/tools/file-manager.lua
+++ .config/nvim/lua/custom/plugins/tools/file-manager.lua
@@ -1,16 +1,5 @@
 return {
   {
-    'stevearc/oil.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    enabled = false,
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
-    keys = {
-      { '<leader>fb', '<cmd>Oil<cr>', desc = '[F]ile [B]rowser' },
-    },
-  },
-  {
     'mikavilpas/yazi.nvim',
     event = 'VeryLazy',
     ---@type YaziConfig | {}
