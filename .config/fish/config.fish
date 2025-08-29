2b38e6ac1bdf49f77a894d09c50447180fbaa39a
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2025-08-29 10:12:41 +0300

    fish: aspire cli

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,6 @@ alias dotfiles='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Added by get-aspire-cli.sh
+fish_add_path $HOME/.aspire/bin
