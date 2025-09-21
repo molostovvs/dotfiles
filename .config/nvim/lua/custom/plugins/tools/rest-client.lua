a9341a1de8e5303e8334c185f9146c2125ee7dab
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2025-09-21 18:37:44 +0300

    nvim(rest-client): migrate to kulala

--- .config/nvim/lua/custom/plugins/tools/rest-client.lua
+++ .config/nvim/lua/custom/plugins/tools/rest-client.lua
@@ -1,10 +1,15 @@
 return {
-  'rest-nvim/rest.nvim',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      table.insert(opts.ensure_installed, 'http')
-    end,
+  {
+    'mistweaverco/kulala.nvim',
+    keys = {
+      { '<leader>Rs', desc = 'Send request' },
+      { '<leader>Ra', desc = 'Send all requests' },
+      { '<leader>Rb', desc = 'Open scratchpad' },
+    },
+    ft = { 'http', 'rest' },
+    opts = {
+      global_keymaps = true,
+      global_keymaps_prefix = '<leader>R',
+    },
   },
 }
