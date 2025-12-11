7705e6f2eb797463909ff9f9c819e5e7ac75383b
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2025-12-11 19:15:52 +0300

    nvim(dap): try to fix vsdbgui debugger strange breakpoins

--- .config/nvim/lua/custom/plugins/debug/dap.lua
+++ .config/nvim/lua/custom/plugins/debug/dap.lua
@@ -52,6 +52,11 @@ return {
         },
       }
 
+      dap.configurations.cs = {
+        stopOnEntry = false,
+        justMyCode = true,
+      }
+
       dap.listeners.after.event_initialized['dapui_config'] = dapui.open
       dap.listeners.before.event_terminated['dapui_config'] = dapui.close
       dap.listeners.before.event_exited['dapui_config'] = dapui.close
