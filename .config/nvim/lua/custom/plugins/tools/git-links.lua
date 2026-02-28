e4fb6e3443b01002ce8a504405fbd28f0ab616f6
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2026-03-01 01:02:17 +0300

    nvim(gitlink): svodka gitlab links

--- .config/nvim/lua/custom/plugins/tools/git-links.lua
+++ .config/nvim/lua/custom/plugins/tools/git-links.lua
@@ -12,6 +12,22 @@ return {
             .. '{_A.FILE}'
             .. '#L{_A.LSTART}'
             .. "{(_A.LEND > _A.LSTART and ('-' .. _A.LEND) or '')}",
+          ['^gitlab%.svodka%.space'] = 'https://gitlab.svodka.space/'
+            .. '{_A.ORG}/'
+            .. '{_A.REPO}/blob/'
+            .. '{_A.CURRENT_BRANCH}/'
+            .. '{_A.FILE}'
+            .. '#L{_A.LSTART}'
+            .. "{(_A.LEND > _A.LSTART and ('-' .. _A.LEND) or '')}",
+        },
+        current_branch = {
+          ['^gitlab%.svodka%.space'] = 'https://gitlab.svodka.space/'
+            .. '{_A.ORG}/'
+            .. '{_A.REPO}/blob/'
+            .. '{_A.CURRENT_BRANCH}/'
+            .. '{_A.FILE}'
+            .. '#L{_A.LSTART}'
+            .. "{(_A.LEND > _A.LSTART and ('-' .. _A.LEND) or '')}",
         },
       },
     },
